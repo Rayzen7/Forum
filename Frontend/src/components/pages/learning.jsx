93d38ebaaa9762696e8d0d6dac9862bdd51768f8
mammkhr <abdulkhoirimam@gmail.com>
2024-07-08 22:42:37 +0700

    first commit

--- Frontend/src/components/pages/learning.jsx
+++ Frontend/src/components/pages/learning.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import './CSS/learning.css'
+import { Link } from 'react-router-dom'
 
 const Learning = () => {
   return (
     <div className='learning' id='learning' data-aos="fade-up">
         <h1>Materi</h1>
         <p>Materi mengenai Organisasi MPK SMK Negeri 26 Jakarta</p>
-        <button><a href="/Learning" style={{textDecoration:"none", color:"black"}}>Belajar</a></button>
+        <Link to="/Learning" style={{textDecoration:"none", color:"black"}}><button>Belajar</button></Link>
     </div>
   )
 }
