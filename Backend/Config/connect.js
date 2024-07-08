8da76a7c7419a65da53cc3a5290afe79e36a2ae7
mammkhr <abdulkhoirimam@gmail.com>
2024-07-08 11:19:36 +0700

    file changes

--- Backend/Config/connect.js
+++ Backend/Config/connect.js
@@ -4,8 +4,8 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const URI = process.env.URI;
-    await mongoose.connect(URI);
+    // const URI = process.env.URI;
+    await mongoose.connect("mongodb+srv://Rayzen7:zhzj0DmXWKRawxOH@rayzen7.mncnnph.mongodb.net/?retryWrites=true&w=majority&appName=Rayzen7");
     console.log("Connection to MongoDB was successful");
   } catch (error) {
     console.error("Failed to connect to MongoDB", error);
