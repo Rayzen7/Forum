f795a058485517c7ba537f9244b09a55dc77d928
mammkhr <abdulkhoirimam@gmail.com>
2024-07-08 11:40:26 +0700

    file changes

--- Backend/Config/connect.js
+++ Backend/Config/connect.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
-dotenv.config();
 
 const connectDB = async () => {
   try {
-    // const URI = process.env.URI;
-    await mongoose.connect("mongodb://Rayzen7:zhzj0DmXWKRawxOH@ac-dob53so-shard-00-00.mncnnph.mongodb.net:27017,ac-dob53so-shard-00-01.mncnnph.mongodb.net:27017,ac-dob53so-shard-00-02.mncnnph.mongodb.net:27017/?ssl=true&replicaSet=atlas-7pnjfm-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Rayzen7");
+    const URI = process.env.URI;
+    await mongoose.connect(URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connection to MongoDB was successful");
   } catch (error) {
     console.error("Failed to connect to MongoDB", error);
