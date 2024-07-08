36887a9509e22fd98c5861121981c9e1db32a06e
mammkhr <abdulkhoirimam@gmail.com>
2024-07-08 21:54:21 +0700

    file changes

--- Backend/Config/connect.js
+++ Backend/Config/connect.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const URI = process.env.URI;
-    await mongoose.connect(URI);
+    // const URI = process.env.URI;
+    await mongoose.connect("mongodb+srv://Rayzen7:zhzj0DmXWKRawxOH@rayzen7.mncnnph.mongodb.net/?retryWrites=true&w=majority&appName=Rayzen7");
     console.log("Connection to MongoDB was successful");
   } catch (error) {
     console.error("Failed to connect to MongoDB", error);
