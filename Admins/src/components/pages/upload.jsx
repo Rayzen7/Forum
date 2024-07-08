1db53cfc3a4cccca20132205393cf27edd846ef0
mammkhr <abdulkhoirimam@gmail.com>
2024-07-08 23:08:32 +0700

    first commit

--- Admins/src/components/pages/upload.jsx
+++ Admins/src/components/pages/upload.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import './CSS/upload.css'
+import { Link } from 'react-router-dom'
 
 const Upload = () => {
   return (
     <div className="uploads">
-      <a href="/Input">
+      <Link to="/Input" style={{textDecoration:"none", color:"white"}}>
         <div className='upload'>
           <i className='bx bx-upload'></i>
           <h1>Upload Materi</h1>
         </div>
-      </a>
+      </Link>
     </div>
   )
 }
